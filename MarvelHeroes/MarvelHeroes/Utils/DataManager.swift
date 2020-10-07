1f9006371b704ac2a9c9530f6998a951a138a700
RodrigoSA <rrodrigo.sa@gmail.com>
2020-10-07 11:48:59 -0700

    Update DataManager

--- MarvelHeroes/MarvelHeroes/Utils/DataManager.swift
+++ MarvelHeroes/MarvelHeroes/Utils/DataManager.swift
@@ -7,4 +7,21 @@
 
 import Foundation
 
+protocol DataDelegate {
+    func didReceive(data: [Character])
+    func didFail(_with error: Error)
+}
+
+public class DataManager {
+    var delegate: DataDelegate?
+    
+    func setData(decodedData: [Character]) -> Void {
+        delegate?.didReceive(data: decodedData)
+    }
+    
+    func setError(error: Error) {
+        delegate?.didFail(_with: error)
+    }
+    
+}
 
