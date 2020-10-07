b6fcf0ba3c4247fce77ffb22aabde783a9a0bc6f
RodrigoSA <rrodrigo.sa@gmail.com>
2020-10-07 16:17:30 -0700

    Update DataManager to get keys from resource bundle

--- MarvelHeroes/MarvelHeroes/Utils/DataManager.swift
+++ MarvelHeroes/MarvelHeroes/Utils/DataManager.swift
@@ -12,9 +12,36 @@ protocol DataDelegate {
     func didFail(_with error: Error)
 }
 
+struct KeyDict {
+    let publicKey: String!
+    let privateKey: String!
+}
+
 public class DataManager {
     var delegate: DataDelegate?
     
+    private var keys: NSDictionary?
+    
+    // get keys on resources bundle
+    func getKeys() -> KeyDict {
+        if let path = Bundle.main.path(forResource: "apikeys", ofType: "plist") {
+            self.keys = NSDictionary(contentsOfFile: path)!
+        }
+        
+        if let data = keys {
+            return KeyDict(publicKey: data["publicKey"] as! String, privateKey: data["privateKey"] as! String)
+        } else {
+            return KeyDict(publicKey: "", privateKey: "")
+        }
+    }
+    
+    // MARK: -> requestData
+    // for online json - TODO
+    func requestData() {
+    }
+    
+    
+    // MARK: -> delegate
     func setData(decodedData: [Character]) -> Void {
         delegate?.didReceive(data: decodedData)
     }
