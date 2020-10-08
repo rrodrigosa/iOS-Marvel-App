096066df99aed80da6e14f093c5269fa62d7a7d7
RodrigoSA <rrodrigo.sa@gmail.com>
2020-10-07 21:45:58 -0700

    Add CharacterCellController

---
+++ MarvelHeroes/MarvelHeroes/Controller/CharacterCellController.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterCellController.swift
+//  MarvelHeroes
+//
+//  Created by RodrigoSA on 10/7/20.
+//
+
+import UIKit
+
+class CharacterCellController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
