d972dd79ffe6fb6a28c45e56bccd2eab53709723
RodrigoSA <rrodrigo.sa@gmail.com>
2020-10-07 22:16:18 -0700

    Remove unused code

--- MarvelHeroes/MarvelHeroes/Controller/CharactersController.swift
+++ MarvelHeroes/MarvelHeroes/Controller/CharactersController.swift
@@ -17,12 +17,6 @@ class CharactersController: UITableViewController {
         self.charactersTableView.tableFooterView = UIView()
         // add view title
         self.title = "Characters List"
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
