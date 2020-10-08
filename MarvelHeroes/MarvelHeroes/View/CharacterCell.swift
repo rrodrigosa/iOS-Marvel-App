f9bcdb16a19e84d29e088778f05609aa576c314c
RodrigoSA <rrodrigo.sa@gmail.com>
2020-10-07 20:57:18 -0700

    Add IBOutlets for CharacterCell

--- MarvelHeroes/MarvelHeroes/View/CharacterCell.swift
+++ MarvelHeroes/MarvelHeroes/View/CharacterCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class CharacterCell: UITableViewCell {
 
+    @IBOutlet weak var charactersContentView: UIView!
+    @IBOutlet weak var charactersImgView: UIImageView!
+    @IBOutlet weak var charactersNameLabel: UILabel!
+    @IBOutlet weak var charactersDescriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
