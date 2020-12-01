cdfdf83a9957a9077546bff6edd5ddea5694bdf1
RodrigoSA <rrodrigo.sa@gmail.com>
2020-12-01 16:10:45 -0300

    Remove unnecessary function

--- MarvelHeroes/MarvelHeroes/Controller/CharactersController.swift
+++ MarvelHeroes/MarvelHeroes/Controller/CharactersController.swift
@@ -33,10 +33,6 @@ class CharactersController: UITableViewController {
     }
     
     // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return charList.count
     }
