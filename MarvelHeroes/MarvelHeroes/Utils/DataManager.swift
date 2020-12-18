56575ba9889c2625c37cdb9801613b7d86123ef3
RodrigoSA <rrodrigo.sa@gmail.com>
2020-12-18 17:11:37 -0300

    Remove ErrorMessage file as it's not reusable, code relocated to DataManager file where it's used

--- MarvelHeroes/MarvelHeroes/Utils/DataManager.swift
+++ MarvelHeroes/MarvelHeroes/Utils/DataManager.swift
@@ -83,3 +83,25 @@ class DataManager {
     
 }
 
+private enum ErrorMessage: Error {
+    case apiNoData
+    case decode
+    case resultNoData
+    case statusCode
+    case noStatusCode
+    
+    var message: String {
+        switch self {
+        case .apiNoData:
+            return "No data received from API".localized
+        case .decode:
+            return "Could not decode API data into characters".localized
+        case .resultNoData:
+            return "No character data received from API".localized
+        case .statusCode:
+            return "Status code: %d".localized
+        case .noStatusCode:
+            return "No status code received".localized
+        }
+    }
+}
